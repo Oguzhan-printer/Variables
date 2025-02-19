200a2402cafdf6d804dc915095144c3e89c5eedb
OĞUZHAN YAZICI <159013707+Oguzhan-printer@users.noreply.github.com>
2025-02-19 08:18:58 +0300

    Add files via upload

---
+++ Variables.py
@@ -0,0 +1,17 @@
+#Task 1
+
+name = str(input('enter your name:'))
+age = int(input('enter your age:'))
+size = float(input('enter your size:'))
+student = bool(input('are you a student? (yes/no):'))
+
+print(f'my name is {name} my age is {age} my height is {size} finally yes I am a student  {student}')
+
+
+#Task 2
+
+birth_day = int(input('enter your birth day:'))
+current_day = 2025
+age = current_day - birth_day
+
+print(f'you are {age} years old')
